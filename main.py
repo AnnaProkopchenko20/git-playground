e9307c449a5845397224742ba51399b01fb75411
Anna Prokopchenko <aprokopchenko@kse.org.ua>
2024-10-29 11:05:50 +0200

    If there is less then 5 words to make from word

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def congratulate_user():
 
 
 def is_game_over():
-    return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
+    return guessed == words_to_win or errors == ERRORS_TO_LOSE
 
 
 guessed = 0
@@ -16,15 +16,14 @@ errors = 0
 
 guesses = []
 
-WORDS_TO_WIN = 5
 ERRORS_TO_LOSE = 3
 
 words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 word = words[random.randrange(0, len(words))]
 full_list = create_full_list(ok_words=words_fetcher.fetch_words(min_letters=3, max_letters=9),word=word)
+words_to_win = min(5, len(full_list))
 
-
-print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Can you make up {words_to_win} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
 
 
@@ -33,10 +32,10 @@ while not is_game_over():
     if guess in full_list and guess not in guesses:
         guessed += 1
         guesses.append(guess)
-        if guessed == WORDS_TO_WIN:
+        if guessed == words_to_win:
             congratulate_user()
             exit()
-        print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+        print(f"That's right! {words_to_win - guessed} to go")
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
